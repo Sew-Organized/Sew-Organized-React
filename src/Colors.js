8442d9b9f948cc2ecd9ac4cc8b5fb90c2bfcbe35
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-03-05 11:46:19 -0800

    added nav bar

--- src/Colors.js
+++ src/Colors.js
@@ -3,6 +3,15 @@ import React, { Component } from 'react';
 import List from './List.js';
 // import Paging from './Paging.js';
 import request from 'superagent';
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import './Nav.css';
+import { 
+    Route, 
+    BrowserRouter as Router, } from 'react-router-dom';
+import Palettes from './Palettes.js';
+import About from './About.js';
+import Stash from './Stash.js';
 
 export default class Colors extends Component {
     // state for the array of flosses to be posted to page
@@ -37,11 +46,54 @@ export default class Colors extends Component {
     render() {
         console.log('state:', this.state);
         return (
+            <>
+            <Router>
+            <Route render={({ location, history }) => (
+                <React.Fragment>
+                    <SideNav className='sideNav'
+                        onSelect={(selected) => {
+                            const to = '/' + selected;
+                            if (location.pathname !== to) {
+                                history.push(to);
+                            }
+                        }}
+                    >
+                        <SideNav.Toggle />
+                        <SideNav.Nav defaultSelected="About">
+                            <NavItem eventKey="About">
+                                <NavIcon>
+                                    <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                                </NavIcon>
+                                <NavText>
+                                    About
+                                </NavText>
+                            </NavItem>
+                            <NavItem eventKey="dStash">
+                                <NavIcon>
+                                    <i className="fa fa-fw fa-device" style={{ fontSize: '1.75em' }} />
+                                </NavIcon>
+                                <NavText>
+                                    My Stash
+                                </NavText>
+                            </NavItem>
+                        </SideNav.Nav>
+                    </SideNav>
+                    <main>
+                        <Route path="/About" exact component={props => <About />} />
+                        <Route path="/user/stash" component={props => <Stash />} />
+                        <Route path="/palettes" component={props => <Palettes />} />
+                    </main>
+                </React.Fragment>
+            )}
+            />
+        </Router>  
+
             <div>
                 {/* <SearchBar /> */}
                 <List handleClick={this.setStash} flosses={this.state.flosses} />
                 {/* <Paging /> */}
             </div>
+            </>
         )
     }
 }
