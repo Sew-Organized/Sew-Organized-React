c088cafdd62ba1b1695fd1f181c006e17076a115
Chelsea Spangler <chelseanspangler@gmail.com>
2020-03-05 11:53:16 -0800

    user's saved palettes render on user/palettes

--- src/SavedPalettes.js
+++ src/SavedPalettes.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import request from 'superagent';
-// import Floss from './Floss.js';
+import Floss from './Floss.js';
 import RandomPalette from './RandomPalette';
+// import UserPalette from './UserPalette.js';
 
 
 export default class SavedPalettes extends Component {
     state = {
         savedPalettes: [],
+        paletteNames: []
     }
 
     // fetch user's saved palettes and store in local state
@@ -17,104 +19,32 @@ export default class SavedPalettes extends Component {
     }
 
     render() {
-        // map over all saved palettes and return a div containing the palette name and the data for each color
-        console.log('saved palettes in state', this.state.savedPalettes)
-        const savedPaletteArray = this.state.savedPalettes.map(palette => {
-            console.log('palette in map', palette)
-            return [palette.palette_name, palette.dmc_one, palette.dmc_two, palette.dmc_three, palette.dmc_four, palette.dmc_five];
-        })
-        console.log('array of arrays??', savedPaletteArray);
+        // const savedPaletteArray = this.state.savedPalettes.map(palette => {
+        //     return [palette.dmc_one, palette.dmc_two, palette.dmc_three, palette.dmc_four, palette.dmc_five];
+        // });
         return(
             <div>
                 <h1>Saved palettes</h1>
                 <div className="paletteContainer">
-                    {
-                        savedPaletteArray.map(palette => {
-                            return(
-                                <div>                                    
-                                    <h2>{palette[0]}</h2>
-                                    <RandomPalette palette={palette} />
-                                    <button>Remove</button>
-                                </div>)
-                        })
-                    }
+                {
+                    // iterate over savedPalettes and return an array of arrays
+
+                    // iterate over the returned array of arrays savedPalettes and for each palette array, show name and render a RandomPalette component
+                    this.state.savedPalettes.map(savedPalette => 
+                        <div className="palette">
+                            { console.log('savedPalette in map', savedPalette) }
+                            <h2>{savedPalette.palette_name}</h2>
+                            <Floss floss={JSON.parse(savedPalette.dmc_one)} />
+                            <Floss floss={JSON.parse(savedPalette.dmc_two)} />
+                            <Floss floss={JSON.parse(savedPalette.dmc_three)} />
+                            <Floss floss={JSON.parse(savedPalette.dmc_four)} />
+                            <Floss floss={JSON.parse(savedPalette.dmc_five)} />
+                            <button>Remove</button>
+                        </div>
+                    )
+                }
                 </div>
             </div>
         )
     }
 }
-
-
-// const mappedPalettes = this.state.savedPalettes.map(savedPalette => 
-    //     <div>
-    //         {console.log(savedPalette)}
-    //         {// savedPalette is being read here}
-    //         <h2>{savedPalette.paletteName}</h2>
-    //         <div className="palette">
-    //             {savedPalette.map(floss => 
-    //                 <Floss floss={floss} />
-    //             )}
-    //         </div>
-
-    //     </div>
-
-        // )
-//         return (
-//             <div>
-//                 <h1>Saved palettes</h1>
-//                 <div className="paletteContainer">
-//                     {/* {mappedPalettes} */}
-            
-//                 {
-//                     // iterate over savedPalettes and return an array of arrays
-
-//                     // iterate over the returned array of arrays savedPalettes and for each palette array, show name and render a RandomPalette component
-//                     this.state.savedPalettes.map(savedPalette => 
-//                         <div className="palette">
-//                         { console.log('savedPalette in map', savedPalette) }
-//                         { this.setState({ savedPalette }) }
-//                         <h2>{savedPalette.palette_name}</h2>
-//                         <RandomPalette palette={this.state.savedPalette} />
-//                          {/* <Floss floss={savedPalette.dmc_one} />
-//                          <Floss floss={savedPalette.dmc_two} />
-//                          <Floss floss={savedPalette.dmc_three} />
-//                          <Floss floss={savedPalette.dmc_four} />
-//                          <Floss floss={savedPalette.dmc_five} /> */}
-//                     </div>
-//                     )
-//             }
-//                 <button>Remove</button>
-//             </div>
-//             </div>
-//         )
-//     }
-// }
-
-    // return (
-    //     <div>
-    //         <h1>Saved palettes</h1>
-    //         <div className="paletteContainer">
-    //             {/* {mappedPalettes} */}
-            
-    //         {
-    //             // iterate over savedPalettes and return an array of arrays
-
-    //             // iterate over the returned array of arrays savedPalettes and for each palette array, show name and render a RandomPalette component
-    //             this.state.savedPalettes.map(savedPalette => 
-    //                 <div className="palette">
-    //                 { console.log('savedPalette in map', savedPalette) }
-    //                 { this.setState({ savedPalette }) }
-    //                 <h2>{savedPalette.palette_name}</h2>
-    //                 <RandomPalette palette={this.state.savedPalette} />
-    //                  {/* <Floss floss={savedPalette.dmc_one} />
-    //                  <Floss floss={savedPalette.dmc_two} />
-    //                  <Floss floss={savedPalette.dmc_three} />
-    //                  <Floss floss={savedPalette.dmc_four} />
-    //                  <Floss floss={savedPalette.dmc_five} /> */}
-    //             </div>
-    //             )
-    //     }
-    //         <button>Remove</button>
-    //     </div>
-    //     </div>
-    // )
---
+++ src/UserPalette.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Floss from './Floss.js';
+
+export default class RandomPalette extends Component {
+    // state = {
+    //     paletteName: ''
+    // }    
+
+    render() {
+        console.log(this.props);
+        return (
+            <div>
+                <div className="paletteContainer">
+                    <div className="palette">
+                        { this.props.palette.map(floss => 
+                        <Floss floss={floss} />
+                    )}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
