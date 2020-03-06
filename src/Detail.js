adc1293d5e4a742c7a072629720c9ecf6fce8f92
Chelsea Spangler <chelseanspangler@gmail.com>
2020-03-06 11:22:47 -0800

    SIMILAR COLORS GENERATOR WORKS!!!!!!!!!!!!!!!!!!!!!

--- src/Detail.js
+++ src/Detail.js
@@ -4,13 +4,16 @@ import Floss from './Floss.js';
 import request from 'superagent';
 import Nav from './Nav.js';
 import Header from './Header.js';
-// import RandomPalette from 'RandomPalette.js';
+import RandomPalette from './RandomPalette.js';
 
 export default class Detail extends Component {
-        state = {
-            floss: {},
-        };
-
+    state = {
+        floss: {},
+        palettes: [],
+        randomPalette: [],
+        dmcData: [],
+    };
+    
     async componentDidMount() {
         const user = JSON.parse(localStorage.getItem('user'));
 
@@ -26,23 +29,91 @@ export default class Detail extends Component {
         this.setState({ dmcData: dmcData.body })
     }
 
+    // generates 5 monochrome dmc colors that are close matches to the current color
+    generateApiColors = async () => {
+        const randomPalette = await request.get(`https://mighty-mesa-93390.herokuapp.com/api/scheme/${this.props.match.params.id}`);
+        this.setState({ 
+            randomPalette: randomPalette.body,
+            buttonDisabled: false
+        });
+        this.dmcMatches();
+    };
+
+    // takes full DMC color object array and transforms into array of arrays
+    transformPaletteData = () => {
+        // turns dmc colors data into arrays for compatibility reasons
+        const dmcColorList = this.state.dmcData.map(color => Object.values(color));
+        return dmcColorList;
+    };
+
+    // take the array of objects returned by generateApiColors
+    dmcMatches = () => {
+        const matchedDMCIds = this.state.randomPalette.map(object => {
+            return this.matchDMC(object.r, object.g, object.b) 
+        })
+        const matchedDMCObjects = matchedDMCIds.map(id => {
+            return this.findById(this.state.dmcData, id);
+        })
+        this.setState({ matchedDMCObjects });
+    }
+
+    // does color math, and is called in matchDMC
+    distanceFromColor(idx, r, g, b) {
+        const dmcColorList = this.transformPaletteData();
+        const tr = dmcColorList[idx][2];
+        const tg = dmcColorList[idx][3];
+        const tb = dmcColorList[idx][4];
+        const baseDistance = ((r - tr) * (r - tr)) + ((g - tg) * (g - tg)) + ((b - tb) * (b - tb));
+        const distance = Math.sqrt(baseDistance);
+        return distance;
+    }
+
+    // where the magic happens
+    matchDMC = (rgbRed, rgbGreen, rgbBlue) => {
+        const dmcColorList = this.transformPaletteData();
+        const distanceList = [];
+        for (let i = 0; i < dmcColorList.length; i++) {
+            const candidateDist = this.distanceFromColor(i, rgbRed, rgbGreen, rgbBlue);
+            distanceList[i] = [candidateDist, i];
+        }
+        // sort the list in ascending order
+        distanceList.sort(function(a, b){return a[0]-b[0]});
+        const dmcI = dmcColorList[distanceList[0][1]][0];
+        return dmcI;
+    }
+
+    findById = (array, id) => {
+        for (let index = 0; index < array.length; index++) {
+            const item = array[index];
+            if (item.id === id) {
+                return item;
+            } 
+        }
+    };
+
     render() {
         return (
             <div>
                 <Header />
                 <Nav />
-                {/* <h1>{ this.state.floss.description }</h1> */}
                 <div>
-                    { this.state.floss 
-                    ? 
                     <div>
                         <Floss floss={ this.state.floss } />
                     </div>
-                    : '' }
                 </div>
-                {/* <ul className='flossDetailContainer'>
-                    <Floss floss={ this.state.floss[0] } />
-                </ul> */}
+                <p>Don't have the color your project calls for? Click below to generate similar DMC colors:</p>
+                <button className="primary" onClick={this.generateApiColors}>Generate Color Matches</button>
+                <div>
+                    {console.log(this.state.matchedDMCObjects)}
+                    { this.state.matchedDMCObjects 
+                    ? 
+                    <div>
+                        <RandomPalette palette={this.state.matchedDMCObjects} />
+                    </div>
+                    : '' 
+                    }
+                    {console.log('matched array in state', this.state.matchedDMCObjects)}
+                </div>
             </div>
         )
     }
--- src/placeholder.js
+++ src/placeholder.js
@@ -7,86 +7,9 @@ import Header from './Header.js';
 // import RandomPalette from 'RandomPalette.js';
 
 export default class Detail extends Component {
-        state = {
-            floss: {},
-            palettes: [],
-            randomPalette: [],
-            dmcData: [],
-        };
+        
 
-    async componentDidMount() {
-        const user = JSON.parse(localStorage.getItem('user'));
-
-        const data = await (await getFloss(this.props.match.params.id, user));
-        if (data.body) {
-            console.log('data.body:', data.body);
-            this.setState({ floss: data.body })
-            console.log('state', this.state.floss);
-        }
-        const dmcData = await request.get(`https://mighty-mesa-93390.herokuapp.com/api/colors`)
-
-        // double check data format that sets state
-        this.setState({ dmcData: dmcData.body })
-    }
-
-        // generates five random colors from color api, converts to nearest DMC colors, and changes format of data
-        generateApiColors = async () => {
-            const randomPalette = await request.get(`https://mighty-mesa-93390.herokuapp.com/api/scheme/:id`);
-            this.setState({ randomPalette: randomPalette.body });
-            this.dmcMatches();
-        };
     
-        // takes full DMC color object array and transforms into array of arrays
-        transformPaletteData = () => {
-            // turns dmc colors data into arrays for compatibility reasons
-            const dmcColorList = this.state.dmcData.map(color => Object.values(color));
-            return dmcColorList;
-        };
-    
-        // take the array of objects returned by generateApiColors
-        dmcMatches = () => {
-            const matchedDMCIds = this.state.randomPalette.map(object => {
-                return this.matchDMC(object.r, object.g, object.b) 
-            })
-            const matchedDMCObjects = matchedDMCIds.map(id => {
-                return this.findById(this.state.dmcData, id);
-            })
-            this.setState({ matchedDMCObjects });
-        }
-    
-        // does color math, and is called in matchDMC
-        distanceFromColor(idx, r, g, b) {
-            const dmcColorList = this.transformPaletteData();
-            const tr = dmcColorList[idx][2];
-            const tg = dmcColorList[idx][3];
-            const tb = dmcColorList[idx][4];
-            const baseDistance = ((r - tr) * (r - tr)) + ((g - tg) * (g - tg)) + ((b - tb) * (b - tb));
-            const distance = Math.sqrt(baseDistance);
-            return distance;
-        }
-    
-        // where the magic happens
-        matchDMC = (rgbRed, rgbGreen, rgbBlue) => {
-            const dmcColorList = this.transformPaletteData();
-            const distanceList = [];
-            for (let i = 0; i < dmcColorList.length; i++) {
-                const candidateDist = this.distanceFromColor(i, rgbRed, rgbGreen, rgbBlue);
-                distanceList[i] = [candidateDist, i];
-            }
-            // sort the list in ascending order
-            distanceList.sort(function(a, b){return a[0]-b[0]});
-            const dmcI = dmcColorList[distanceList[0][1]][0];
-            return dmcI;
-        }
-    
-        findById = (array, id) => {
-            for (let index = 0; index < array.length; index++) {
-                const item = array[index];
-                if (item.id === id) {
-                    return item;
-                } 
-            }
-        };
 
     render() {
         return (
@@ -96,18 +19,7 @@ export default class Detail extends Component {
                 <ul className='flossDetailContainer'>
                     <Floss floss={ this.state.floss } />
                 </ul>
-                <p>Don't have the color your project calls for? Click below to generate similar DMC colors:</p>
-                <button className="primary" onClick={this.generateApiColors}>Generate Color Matches</button>
-                <div>
-                    { this.state.matchedDMCObjects 
-                    ? 
-                    <div>
-                        {/* <RandomPalette palette={this.state.matchedDMCObjects} /> */}
-                    </div>
-                    : '' 
-                    }
-                    {console.log('matched array in state', this.state.matchedDMCObjects)}
-                </div>
+                
             </div>
         )
     }
