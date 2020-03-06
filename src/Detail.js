e08bf3df72cbed28e4dd5bcf3d50cb08657abba2
Chelsea Spangler <chelseanspangler@gmail.com>
2020-03-06 11:29:07 -0800

    style detail page

--- src/Detail.js
+++ src/Detail.js
@@ -93,7 +93,7 @@ export default class Detail extends Component {
 
     render() {
         return (
-            <div>
+            <div className="componentContainer">
                 <Header />
                 <Nav />
                 <div>
