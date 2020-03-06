a33115c127412bf48dd8dd29852ca03f5d72d461
Dannie Schumaker <draeschumaker@gmail.com>
2020-03-06 01:04:13 -0800

    modified palette style

--- src/RandomPalette.js
+++ src/RandomPalette.js
@@ -30,8 +30,10 @@ export default class RandomPalette extends Component {
             <div className="componentContainer">
                 <Nav />
                 <div className="paletteContainer">
-                    <input placeholder="Name Your Palette" value={this.state.paletteName} onChange={(e) => this.setState({ paletteName: e.target.value })} />
-                        <button className="primary" onClick={this.savePalette}>Save Palette to Stash</button>
+                    <div className="flexRightAlign">
+                        <input placeholder="Name Your Palette" value={this.state.paletteName} onChange={(e) => this.setState({ paletteName: e.target.value })} />
+                        <button className="primary" onClick={this.savePalette}>Save</button>
+                    </div>
                     <div className="palette">
                         { this.props.palette.map(floss => 
                         <Floss floss={floss} />
