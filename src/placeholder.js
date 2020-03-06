db88e736bfedc4c3344a94f099f3098e0a6c654f
Chelsea Spangler <chelseanspangler@gmail.com>
2020-03-05 17:19:30 -0800

    detail page almost working

--- src/placeholder.js
+++ src/placeholder.js
@@ -1,128 +1,111 @@
-
-
-
-
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { getFloss } from './services/API.js';
+import Floss from './Floss.js';
 import request from 'superagent';
-// import Floss from './Floss.js';
-import RandomPalette from './RandomPalette';
-import UserPalette from './UserPalette.js';
+// import RandomPalette from 'RandomPalette.js';
 
+export default class Detail extends Component {
+        state = {
+            floss: {},
+            palettes: [],
+            randomPalette: [],
+            dmcData: [],
+        };
 
-export default class SavedPalettes extends Component {
-    state = {
-        savedPalettes: [],
-        paletteNames: []
-    }
-
-    // fetch user's saved palettes and store in local state
-    componentDidMount = async () => {
+    async componentDidMount() {
         const user = JSON.parse(localStorage.getItem('user'));
-        const savedPalettesData = await request.get(`https://mighty-mesa-93390.herokuapp.com/api/username/palettes`).set('Authorization', user.token);
-        this.setState({ savedPalettes: savedPalettesData.body });
+
+        const data = await (await getFloss(this.props.match.params.id, user));
+        if (data.body) {
+            console.log(data.body);
+            this.setState({ floss: data.body })
+        }
+        // put in api request link for rendering data
+        const palettesData = await request.get(`https://mighty-mesa-93390.herokuapp.com/api/username/palettes
+        `).set('Authorization', user.token);
+
+        const dmcData = await request.get(`https://mighty-mesa-93390.herokuapp.com/api/colors`)
+
+        // double check data format that sets state
+        this.setState({ palettes: palettesData.body, dmcData: dmcData.body })
     }
+
+        // generates five random colors from color api, converts to nearest DMC colors, and changes format of data
+        generateApiColors = async () => {
+            const randomPalette = await request.get(`https://mighty-mesa-93390.herokuapp.com/api/scheme`);
+            this.setState({ randomPalette: randomPalette.body });
+            this.dmcMatches();
+        };
+    
+        // takes full DMC color object array and transforms into array of arrays
+        transformPaletteData = () => {
+            // turns dmc colors data into arrays for compatibility reasons
+            const dmcColorList = this.state.dmcData.map(color => Object.values(color));
+            return dmcColorList;
+        };
+    
+        // take the array of objects returned by generateApiColors
+        dmcMatches = () => {
+            const matchedDMCIds = this.state.randomPalette.map(object => {
+                return this.matchDMC(object.r, object.g, object.b) 
+            })
+            const matchedDMCObjects = matchedDMCIds.map(id => {
+                return this.findById(this.state.dmcData, id);
+            })
+            this.setState({ matchedDMCObjects });
+        }
+    
+        // does color math, and is called in matchDMC
+        distanceFromColor(idx, r, g, b) {
+            const dmcColorList = this.transformPaletteData();
+            const tr = dmcColorList[idx][2];
+            const tg = dmcColorList[idx][3];
+            const tb = dmcColorList[idx][4];
+            const baseDistance = ((r - tr) * (r - tr)) + ((g - tg) * (g - tg)) + ((b - tb) * (b - tb));
+            const distance = Math.sqrt(baseDistance);
+            return distance;
+        }
+    
+        // where the magic happens
+        matchDMC = (rgbRed, rgbGreen, rgbBlue) => {
+            const dmcColorList = this.transformPaletteData();
+            const distanceList = [];
+            for (let i = 0; i < dmcColorList.length; i++) {
+                const candidateDist = this.distanceFromColor(i, rgbRed, rgbGreen, rgbBlue);
+                distanceList[i] = [candidateDist, i];
+            }
+            // sort the list in ascending order
+            distanceList.sort(function(a, b){return a[0]-b[0]});
+            const dmcI = dmcColorList[distanceList[0][1]][0];
+            return dmcI;
+        }
+    
+        findById = (array, id) => {
+            for (let index = 0; index < array.length; index++) {
+                const item = array[index];
+                if (item.id === id) {
+                    return item;
+                } 
+            }
+        };
 
     render() {
-        // map over all saved palettes and return a div containing the palette name and the data for each color
-        const savedPaletteArray = this.state.savedPalettes.map(palette => {
-            return [palette.dmc_one, palette.dmc_two, palette.dmc_three, palette.dmc_four, palette.dmc_five];
-        });
-
-        const savedPaletteNames = this.state.savedPalettes.map(palette => {
-            return palette.palette_name
-        })
-
-        return(
+        return (
             <div>
-                <h1>Saved palettes</h1>
-                <div className="paletteContainer">
-                    {
-                        savedPaletteArray.map(palette => {
-                            return(
-                                <div>                                    
-                                    <h2>{this.state.paletteName}</h2>
-                                    <UserPalette palette={palette} />
-                                    <button>Remove</button>
-                                </div>)
-                        })
-                    }
+                <ul className='flossDetailContainer'>
+                    <Floss floss={ this.state.floss } />
+                </ul>
+                <p>Don't have the color your project calls for? Click below to generate similar DMC colors:</p>
+                <button onClick={this.generateApiColors}>Generate Color Matches</button>
+                <div>
+                    { this.state.matchedDMCObjects 
+                    ? 
+                    <div>
+                        {/* <RandomPalette palette={this.state.matchedDMCObjects} /> */}
+                    </div>
+                    : '' }
                 </div>
             </div>
         )
     }
 }
-
-
-// const mappedPalettes = this.state.savedPalettes.map(savedPalette => 
-    //     <div>
-    //         {console.log(savedPalette)}
-    //         {// savedPalette is being read here}
-    //         <h2>{savedPalette.paletteName}</h2>
-    //         <div className="palette">
-    //             {savedPalette.map(floss => 
-    //                 <Floss floss={floss} />
-    //             )}
-    //         </div>
-
-    //     </div>
-
-        // )
-//         return (
-//             <div>
-//                 <h1>Saved palettes</h1>
-//                 <div className="paletteContainer">
-//                     {/* {mappedPalettes} */}
-            
-//                 {
-//                     // iterate over savedPalettes and return an array of arrays
-
-//                     // iterate over the returned array of arrays savedPalettes and for each palette array, show name and render a RandomPalette component
-//                     this.state.savedPalettes.map(savedPalette => 
-//                         <div className="palette">
-//                         { console.log('savedPalette in map', savedPalette) }
-//                         { this.setState({ savedPalette }) }
-//                         <h2>{savedPalette.palette_name}</h2>
-//                         <RandomPalette palette={this.state.savedPalette} />
-//                          {/* <Floss floss={savedPalette.dmc_one} />
-//                          <Floss floss={savedPalette.dmc_two} />
-//                          <Floss floss={savedPalette.dmc_three} />
-//                          <Floss floss={savedPalette.dmc_four} />
-//                          <Floss floss={savedPalette.dmc_five} /> */}
-//                     </div>
-//                     )
-//             }
-//                 <button>Remove</button>
-//             </div>
-//             </div>
-//         )
-//     }
-// }
-
-    // return (
-    //     <div>
-    //         <h1>Saved palettes</h1>
-    //         <div className="paletteContainer">
-    //             {/* {mappedPalettes} */}
-            
-    //         {
-    //             // iterate over savedPalettes and return an array of arrays
-
-    //             // iterate over the returned array of arrays savedPalettes and for each palette array, show name and render a RandomPalette component
-    //             this.state.savedPalettes.map(savedPalette => 
-    //                 <div className="palette">
-    //                 { console.log('savedPalette in map', savedPalette) }
-    //                 { this.setState({ savedPalette }) }
-    //                 <h2>{savedPalette.palette_name}</h2>
-    //                 <RandomPalette palette={this.state.savedPalette} />
-    //                  {/* <Floss floss={savedPalette.dmc_one} />
-    //                  <Floss floss={savedPalette.dmc_two} />
-    //                  <Floss floss={savedPalette.dmc_three} />
-    //                  <Floss floss={savedPalette.dmc_four} />
-    //                  <Floss floss={savedPalette.dmc_five} /> */}
-    //             </div>
-    //             )
-    //     }
-    //         <button>Remove</button>
-    //     </div>
-    //     </div>
-    // )
